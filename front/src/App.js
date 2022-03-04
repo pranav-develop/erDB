1d5801b4de1642322540a19ed9c62a95cbd8c979
tushar5526 <codingid6@gmail.com>
2022-03-05 03:21:40 +0530

    basic setup to get started

--- front/src/App.js
+++ front/src/App.js
@@ -4,11 +4,28 @@ import "./Responsive.css";
 import { Route, Routes } from "react-router-dom";
 import { RouteTypes } from "./constants/Routes";
 import Home from "./pages/Home";
+import { useRete } from "./rete";
+
+
+
+function Editor() {
+    const [setContainer] = useRete();
+
+    return (
+        <div
+            style={{
+                width: "100vw",
+                height: "100vh"
+            }}
+            ref={(ref) => ref && setContainer(ref)}
+        />
+    );
+}
 
 function App() {
     return (
         <Routes>
-            <Route path={RouteTypes.HOME_ROUTE} element={Home} />
+            <Route path={RouteTypes.HOME_ROUTE} element={<Editor />} />
         </Routes>
     );
 }
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,8 +1,30 @@
 //jshint esversion: 9
 import React from "react";
+import Rete from "rete";
+import ConnectionPlugin from 'rete-connection-plugin';
+import ReactRenderPlugin from "rete-react-render-plugin";
+
+const numSocket = new Rete.Socket('Number value');
+
+class NumComponent extends Rete.Component {
+    constructor() {
+        super('Number');
+    }
+
+    builder(node) {
+        let out = new Rete.Output('num', 'Number', numSocket);
+
+        node.addOutput(out);
+    }
+
+    worker(node, inputs, outputs) {
+        outputs['num'] = node.data.num;
+    }
+}
 
 function Home() {
-    return <div>Hello world</div>;
+
+    return (<NumComponent />);
 }
 
 export default Home;
