30bb4566ff8f021e9df049475963915b779c719a
tushar5526 <codingid6@gmail.com>
2022-03-21 02:12:28 +0530

    added a multiply component

--- front/src/rete.jsx
+++ front/src/rete.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback, useRef } from "react";
 import Rete from "rete";
 import ReactRenderPlugin from "rete-react-render-plugin";
 import ConnectionPlugin from "rete-connection-plugin";
+import ContextMenuPlugin from "rete-context-menu-plugin";
 import AreaPlugin from "rete-area-plugin";
 
 var numSocket = new Rete.Socket("Number value");
@@ -94,12 +95,47 @@ class AddComponent extends Rete.Component {
     }
 }
 
+class MultiplyComponent extends Rete.Component {
+    constructor() {
+        super("Multiply");
+    }
+
+    builder(node) {
+        var inp1 = new Rete.Input("num1", "Number", numSocket);
+        var inp2 = new Rete.Input("num2", "Number2", numSocket);
+        var out = new Rete.Output("num", "Number", numSocket);
+
+        inp1.addControl(new NumControl(this.editor, "num1", node));
+        inp2.addControl(new NumControl(this.editor, "num2", node));
+
+        return node
+            .addInput(inp1)
+            .addInput(inp2)
+            .addControl(new NumControl(this.editor, "preview", node, true))
+            .addOutput(out);
+    }
+
+    worker(node, inputs, outputs) {
+        var n1 = inputs["num1"].length ? inputs["num1"][0] : node.data.num1;
+        var n2 = inputs["num2"].length ? inputs["num2"][0] : node.data.num2;
+        var mul = n1 * n2;
+
+        this.editor.nodes
+            .find((n) => n.id === node.id)
+            .controls.get("preview")
+            .setValue(mul);
+        outputs["num"] = mul;
+    }
+}
+
 export async function createEditor(container) {
-    var components = [new NumComponent(), new AddComponent()];
+    console.log("called", Date.now())
+    var components = [new NumComponent(), new AddComponent(), new MultiplyComponent()];
 
     var editor = new Rete.NodeEditor("demo@0.1.0", container);
     editor.use(ConnectionPlugin);
     editor.use(ReactRenderPlugin);
+    editor.use(ContextMenuPlugin);
 
     var engine = new Rete.Engine("demo@0.1.0");
 
